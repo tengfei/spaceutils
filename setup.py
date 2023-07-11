a05d4aaf249b4eee2de0f2dfa3685bcd040efad7
tengfei <yintengfei@gmail.com>
2023-07-10 23:47:33 -0400

    initial commit structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spaceutils',
+    version='0.1',
+    packages=find_packages(),
+    description='A utility package for squarespace that not covered by squarespace api',
+    author='Tengfei Yin',
+    author_email='yintengfei@gmail.com',
+    url='https://github.com/tengfei/spaceutils',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
